b017993dde97a98fa3956d50157aff3e82110d5f
Yamingue <yamking01@gmail.com>
2022-03-15 00:51:23 +0100

    PRERAMBULE

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import NavBar from "./Component/NavBar";
 import IMG1 from './assets/1.jpg'
 import Titre from "./Component/Titre";
 import PersonCard from "./Component/PersonCard";
-import { Container, Grid } from "@material-ui/core";
+import { Card, CardContent, Container, Grid } from "@material-ui/core";
 import Mission from "./Component/Mission";
 import ContactForm from "./Component/ContactForm";
 import AdhesionForm from "./Component/AdhesionForm";
@@ -20,6 +20,17 @@ function App() {
         }} />
 
       <Container>
+        <Titre text="Préambule" />
+        <Card>
+          <CardContent>
+            La position géopolitique du Tchad le place au carrefour de multiples pôles de crises climatique et sécuritaires régionaux affectant sa population, surtout celles vivants dans les localités frontalières et difficiles d’accès. Cette position qui confère au pays un rôle de partenaire de première importance pour assurer la stabilité sécuritaire dans la région n’est pas sans conséquence sur la vie sociale au niveau interne. En effet,  cette sûreté exige l´adoption d´une approche intégrée axée sur les notions interconnectées de la paix, de la sécurité humaine et environnementale, de la sécurité économique (moyens de subsistances) et alimentaire, l’accès à l’éducation, bref de la protection sociale.
+            Ces défis sociétaux, induits par des crises humanitaires, ont des contours aussi multiples que variés qui peuvent être appréhendés par la recherche scientifique.  Il n’est pas superflu de rappeler la place de choix qu’occupe la recherche dans tout processus de développement humain.
+            Il est donc nécessaire et judicieux que le Tchad soit doté d’une structure de recherche capable l’aider à s’inscrire dans ce processus global qu’est la promotion de la recherche scientifique et surtout la recherche orientée vers le l’humanitaire.
+            L’intérêt est sans nul doute encore grand pour le Tchad qui, depuis l’enchainement des crises sécuritaires dans la Sous-région, constitue un pilier de la lutte contre l’insécurité et amorce une phase décisive du dialogue orienté vers le développement humain.
+            Au regard de ce constat et conscient de nos modestes capacités et compétences susceptibles de contribuer à cette lutte commune, NOUS, Jeunes Chercheurs et Etudiants, décidons de la création d’une association de recherche dénommée Centre pour la Recherche Humanitaire, en abrégé CRH, avec une vision pluridisciplinaire et orientée vers la promotion de la recherche pour contribuer aux prises des décisions humanitaires au Tchad.
+
+          </CardContent>
+        </Card>
         <Titre text="Missions" size={93} id='mission' />
         <Grid
           container
